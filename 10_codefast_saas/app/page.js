6f2bbde8a0ffe686b2469530641297c64746df4e
Victor Costa Nova <64611385+victorcostanova@users.noreply.github.com>
2025-02-17 20:58:24 -0800

    tailwind

--- 10_codefast_saas/app/page.js
+++ 10_codefast_saas/app/page.js
@@ -5,11 +5,15 @@ export default function Home() {
   const isLoggedIn = true;
   return (
     <main>
-      <h1>Collect customer feedback yo build better products</h1>
-      <div>
-        Create a feedback board in minutes, prioritize features, and build
-        products your customers will love.
-      </div>
+      <section className="text-center px-8 py-32 max-w-3xl mx-auto">
+        <h1 className="text-4xl font-extrabold">
+          Collect customer feedback you build better products
+        </h1>
+        <div className="opacity-90 mb-10">
+          Create a feedback board in minutes, prioritize features, and build
+          products y our customers will love.
+        </div>
+      </section>
 
       <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
     </main>
