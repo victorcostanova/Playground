300371e8e3141b5f73947a746360f1e74ef9ed64
Victor Costa Nova <64611385+victorcostanova@users.noreply.github.com>
2025-03-10 20:46:04 -0700

    background

--- docs/12_mariotest/js/script.js
+++ docs/12_mariotest/js/script.js
@@ -20,6 +20,12 @@ const loop = setInterval(() => {
 
     mario.style.animation = "none";
     mario.style.bottom = `${marioPosition}px`;
+
+    mario.src = "./images/game-over.png";
+    mario.style.width = "80px";
+    mario.style.marginLeft = "50px";
+
+    clearInterval(loop);
   }
 }, 10);
 
