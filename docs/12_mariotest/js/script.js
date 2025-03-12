167f078d681f6e089a23d278afe383afa98cb7be
Victor Costa Nova <64611385+victorcostanova@users.noreply.github.com>
2025-03-11 18:36:21 -0700

    gameover

--- docs/12_mariotest/js/script.js
+++ docs/12_mariotest/js/script.js
@@ -1,5 +1,6 @@
 const mario = document.querySelector(".mario");
 const pipe = document.querySelector(".pipe");
+const clouds = document.querySelector(".clouds");
 
 const jump = () => {
   mario.classList.add("jump");
@@ -10,11 +11,16 @@ const jump = () => {
 
 const loop = setInterval(() => {
   const pipePosition = pipe.offsetLeft;
+  const cloudsPosition = clouds.offsetLeft;
   const marioPosition = +window
     .getComputedStyle(mario)
     .bottom.replace("px", "");
 
   if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 88) {
+    document.getElementById("gameover").style.display = "block";
+    clouds.style.animation = "none";
+    clouds.style.left = `${cloudsPosition}px`;
+
     pipe.style.animation = "none";
     pipe.style.left = `${pipePosition}px`;
 
