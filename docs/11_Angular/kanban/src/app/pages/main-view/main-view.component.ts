6483d4a570ab927c31039a73117f492624b7f3bf
Victor Costa Nova <64611385+victorcostanova@users.noreply.github.com>
2025-03-13 22:02:21 -0700

    background

---
+++ docs/11_Angular/kanban/src/app/pages/main-view/main-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-view',
+  imports: [],
+  templateUrl: './main-view.component.html',
+  styleUrl: './main-view.component.scss'
+})
+export class MainViewComponent {
+
+}
