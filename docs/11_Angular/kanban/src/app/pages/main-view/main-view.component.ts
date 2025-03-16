e194c06414b66de72511c2a89908c13c85d853d0
Victor Costa Nova <64611385+victorcostanova@users.noreply.github.com>
2025-03-15 20:02:21 -0700

    drag&drop

--- docs/11_Angular/kanban/src/app/pages/main-view/main-view.component.ts
+++ docs/11_Angular/kanban/src/app/pages/main-view/main-view.component.ts
@@ -1,11 +1,58 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Board } from '../../models/board.model';
+import { Column } from '../../models/column.model';
+
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+  CdkDropList,
+  DragDropModule,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-main-view',
-  imports: [],
+  imports: [NgFor, DragDropModule],
   templateUrl: './main-view.component.html',
-  styleUrl: './main-view.component.scss'
+  styleUrl: './main-view.component.scss',
 })
 export class MainViewComponent {
+  constructor() {}
 
+  board: Board = new Board('test Board', [
+    new Column('Ideas', ['Some random idea', 'Vai tomando', 'BTC']),
+    new Column('Research', ['Vai tomando', 'RX', 'Siiiiu']),
+    new Column('Todo', [
+      'Get to work',
+      'Pick up groceries',
+      'Go home',
+      'Fall asleep',
+    ]),
+    new Column('Done', [
+      'Get up',
+      'Brush teeth',
+      'Take a shower',
+      'Check e-mail',
+      'Walk dog',
+    ]),
+  ]);
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
 }
