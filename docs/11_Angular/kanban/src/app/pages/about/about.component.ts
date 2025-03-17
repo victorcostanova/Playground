d8ce143710e569622d5e07f7c8d6290e2558868f
Victor Costa Nova <64611385+victorcostanova@users.noreply.github.com>
2025-03-16 20:35:35 -0700

    about

--- docs/11_Angular/kanban/src/app/pages/about/about.component.ts
+++ docs/11_Angular/kanban/src/app/pages/about/about.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+  ],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
 })
 export class AboutComponent {
+  constructor(private router: Router) {}
 
+  navigateTo(path: string) {
+    this.router.navigate([path]);
+  }
 }
