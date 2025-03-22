c61e506e3dafed65f8206bd2100d2cb3ef10fe7a
Victor Costa Nova <64611385+victorcostanova@users.noreply.github.com>
2025-03-21 18:34:48 -0700

    firebase_kanban

---
+++ docs/11_Angular/firebase_kanban/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'firebase_kanban';
+
+  constructor(private router: Router) {}
+
+  // Método para navegação
+  navigateTo(route: string) {
+    this.router.navigate([route]);
+  }
+}
---
+++ docs/11_Angular/firebase_kanban/src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TasksComponent } from './pages/tasks/tasks.component';
+import { AboutComponent } from './pages/about/about.component';
+
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'tasks', component: TasksComponent },
+];
